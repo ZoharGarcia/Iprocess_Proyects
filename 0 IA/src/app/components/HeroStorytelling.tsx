7f507d98f975d7c1f7e7a7625324b54048ee6736
ZoharGarcia <garciabrisa2007@gmail.com>
2026-02-10 13:54:52 -0600

    Storytelling

---
+++ 0 IA/src/app/components/HeroStorytelling.tsx
@@ -0,0 +1,7 @@
+import { ArrowRight, CheckCircle2, Users, Globe } from "lucide-react";
+import { Button } from "@/app/components/ui/button";
+import { motion } from "motion/react";
+import { ImageWithFallback } from "@/app/components/figma/ImageWithFallback";
+import { Link } from "react-router-dom";
+import recursoPartner from "@/assets/img/PortadaUnidades.jpeg"; 
+
