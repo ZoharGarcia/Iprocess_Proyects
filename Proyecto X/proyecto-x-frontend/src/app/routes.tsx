97c0ae5e83dea06596aa8dda4bc46198b08997c4
ZoharGarcia <garciabrisa2007@gmail.com>
2026-02-12 11:49:46 -0600

    feat: add Login and Register pages with routing

--- Proyecto X/proyecto-x-frontend/src/app/routes.tsx
+++ Proyecto X/proyecto-x-frontend/src/app/routes.tsx
@@ -1,12 +1,20 @@
 import { createBrowserRouter } from "react-router";
 import { RootLayout } from "./layouts/RootLayout";
+
 import { Home } from "./pages/Home";
 import { FeaturesPage } from "./pages/FeaturesPage";
 import { PricingPage } from "./pages/PricingPage";
 import { ContactPage } from "./pages/ContactPage";
 import { NotFound } from "./pages/NotFound";
 
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+
 export const router = createBrowserRouter([
+  
+  { path: "/login", Component: Login },
+  { path: "/register", Component: Register },
+
   {
     path: "/",
     Component: RootLayout,
